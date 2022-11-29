cf62ef9ea5d55a61259795eca70f4f8d526708e1
jose marcos luna <marcoslunahbl@gmail.com>
2022-11-29 13:03:52 -0400

    Haciendo pruebas

--- wetaca/lib/cubit_clientes/clientes_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'clientes_state.dart';
-
-class ClientesCubit extends Cubit<ClientesState> {
-  ClientesCubit() : super(ClientesInitial());
-}
--- wetaca/lib/main.dart
+++ wetaca/lib/main.dart
@@ -11,20 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Proyecto Wetaca',
+      title: 'proyecto wetaca',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Proyecto Wetaca'),
+      home: const MyHomePage(title: 'BIENVENIDO A WETACA'),
     );
   }
 }
@@ -38,9 +29,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// clase que contiene el estado de la aplicación
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+// Funcion para incrementar el contador
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
